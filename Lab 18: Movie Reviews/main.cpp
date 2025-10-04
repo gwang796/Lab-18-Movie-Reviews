4d01ec398f80192b2faec9d800467aefb64dcb4d
Guo An Wang <you@example.com>
2025-10-04 14:02:27 -0700

    second commit

--- Lab 18: Movie Reviews/main.cpp
+++ Lab 18: Movie Reviews/main.cpp
@@ -6,10 +6,33 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+struct Movie{
+    double rating;
+    string comment;
+    Movie *next;
+    
+};
+
+const int SIZE = 3;
+const int WIDTH = 5;
+
+void addFront(Movie *&, int, string);
+void addBack(Movie *&, int, string);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Movie *head = nullptr;
+    
+    int user;
+    cout << "Which linked list method should we use?" << endl;
+    cout << setw(WIDTH) << "[1] New nodes are added at the head of the linked list" << endl;
+    cout << setw(WIDTH) << "[2] New nodes are added at the tail of the linked list" << endl;
+    cout << setw(WIDTH) << "Choice: ";
+    cin >> user;
+    cin.ignore();
+    if (user==1) {
+        //addFront()
+    }
 }
