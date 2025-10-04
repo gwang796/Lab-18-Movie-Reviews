782a3cae505e2ae3af3cac87d56b1649116cfe04
Guo An Wang <you@example.com>
2025-10-04 13:50:18 -0700

    Initial commit

---
+++ Lab 18: Movie Reviews/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMSC-210 | Lab 18: Movie Reviews | Guo An Wang
+//  IDE: Xcode
+//  Created by Guo An Wang on 10/4/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
